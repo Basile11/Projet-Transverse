98674ebd33eb411fcd88587a0efc48bc0d1e4713
PaladinSkyland <75802307+PaladinSkyland@users.noreply.github.com>
2023-05-17 20:08:36 +0200

    Auth page is good

--- src/components/Firebase/signin.js
+++ src/components/Firebase/signin.js
@@ -39,60 +39,16 @@ import {
 
 const auth = firebase.auth()
 
-
 export default class Signin extends React.Component {
+  
   
   render () {
     return (
       <View style={style.petitbouton}>
-          <Button onPress={() => this.fonctionfirebasesign()} title="Sign in"/>
           <Button onPress={() => this.fonctionfirebasesignout()} title="Sign out"/>
         </View>
     )
   }
-
-  fonctionfirebasesign() {
-    
-    // s'authentifier pour la premier fois
-
-    /*
-    auth.createUserWithEmailAndPassword('jane.doe@example.com', 'SuperSecretPassword!')
-  .then(() => {
-    console.log('User account created & signed in!');
-  })
-  .catch(error => {
-    if (error.code === 'auth/email-already-in-use') {
-      console.log('That email address is already in use!');
-    }
-
-    if (error.code === 'auth/invalid-email') {
-      console.log('That email address is invalid!');
-    }
-
-    console.error(error);
-  });*/
-
-  // Fonction d'authentification
-
-
-const signIn = (email, password) => {
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // Authentification réussie
-      const user = userCredential.user;
-      console.log('Utilisateur connecté:', user.uid);
-    })
-    .catch((error) => {
-      // Gestion des erreurs
-      console.log('Erreur d\'authentification:', error);
-    });
-};
-signIn('jane.doe@example.com', 'SuperSecretPassword!');
-console.log(firebase.auth().currentUser)
-}
-
 
   fonctionfirebasesignout() {
 
@@ -102,6 +58,8 @@ console.log(firebase.auth().currentUser)
   .then(() => {
     // Déconnexion réussie
     console.log('Utilisateur déconnecté');
+    
+    // navigation.navigate('Auth');
   })
   .catch((error) => {
     // Gestion des erreurs
--- src/screen/Auth/Auth.js
+++ src/screen/Auth/Auth.js
@@ -1,35 +1,91 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigation } from '@react-navigation/core';
 import { StyleSheet, Text, View, Dimensions, StatusBar, TextInput, TouchableOpacity } from 'react-native';
 
+import { firebase, app } from '../../../config.js'
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
 import Footer from '../../components/Footer/Footer.js';
-// import Signin from '../../components/Firebase/signin.js';
+
+
+
 
 
 export default function Auth() {
   const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const { width, height } = Dimensions.get('window');
 
-  const Signin = async() => {
-    try {
-        navigation.navigate('Home');
-    }
-    catch (e) {
-        console.log(e);
-    }
-    };
+  const handleEmailChange = (value) => {
+    setEmail(value);
+  };
 
-    const Signup = async() => {
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+  };
+
+
+  //BUTTON SIGNIN
+  const handleButtonSignin = () => {
+    signInfirebase(email,password)
+  };
+
+  //BUTTON SIGNUP
+  const handleButtonSignup = () => {
+    authfirebase(email,password)
+  };
+
+
+  const signInfirebase = (email, password) => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Authentification réussie
+        const user = userCredential.user;
+        console.log('Utilisateur connecté:', user.uid);
         try {
-            navigation.navigate('Home');
-        }
-        catch (e) {
-            console.log(e);
-        }
-    };
+          navigation.navigate('Home');
+      }
+      catch (e) {
+          console.log(e);
+      }
+      })
+      .catch((error) => {
+        // Gestion des erreurs
+        setErrorMessage('Erreur d\'authentification', error);
+        console.log("naaaaa")
+      });
+  };
+  
+
+  const authfirebase = (email,password) => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then(() => {
+    console.log('User account created & signed in!');
+    signInfirebase(email,password)
+  })
+  .catch(error => {
+    if (error.code === 'auth/email-already-in-use') {
+      setErrorMessage('That email address is already in use!');
+    }
+
+    if (error.code === 'auth/invalid-email') {
+      setErrorMessage('That email address is invalid!');
+    }
+
+    console.error(error);
+  });
+  }
 
 
+
+
+  //STYLE
   const styles = StyleSheet.create({
     container: {
         backgroundColor : 'black',
@@ -85,13 +141,14 @@ export default function Auth() {
         fontWeight: 'bold',
         color: 'white',
         },
-
-
-
-
+      whiteText: {
+        color: 'white',
+      },
   
   });
   
+
+
 
 
   return (
@@ -100,14 +157,26 @@ export default function Auth() {
 
         <Text style={styles.gestock}>Gestock</Text>
         
-        <TextInput style={styles.email}> Email </TextInput>
-        <TextInput style={styles.email}> Mot de passe </TextInput>
+        {errorMessage !== '' && <Text style={styles.whiteText}>{errorMessage}</Text>}
+
+        <TextInput style={styles.email}
+        value={email}
+        onChangeText={handleEmailChange}
+        placeholder="Email"/>
+        
+
+        <TextInput style={styles.email}
+        value={password}
+        onChangeText={handlePasswordChange}
+        placeholder="Mot de passe"
+        secureTextEntry/>
 
         <View style={styles.viewbutton}>
-            <TouchableOpacity onPress={Signin} style={styles.button}>
+            <TouchableOpacity onPress={handleButtonSignin} style={styles.button}>
                 <Text style={styles.textbut}>Sign in</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={Signup} style={styles.button}>
+
+            <TouchableOpacity onPress={handleButtonSignup} style={styles.button}>
                 <Text style={styles.textbut}>Sign up</Text>
             </TouchableOpacity>
         </View>
